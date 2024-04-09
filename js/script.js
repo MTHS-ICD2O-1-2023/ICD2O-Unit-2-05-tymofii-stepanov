95bb4eef19b96af5855f8bc7a4ad33ba4ebb5e3f
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-09 18:13:59 +0000

    done work

--- js/script.js
+++ js/script.js
@@ -9,12 +9,12 @@ function calculatePayAndTaxes () {
   // input
   const numberOfHoursYouWorked = parseInt(document.getElementById('number-of-hours-you-worked').value)
   const timesYourSalary= parseInt(document.getElementById('times-your-salary').value)
-
+  const numberOfTax = 0.18;
   // process
-  const pay = Math.floor(numberOfHoursYouWorked * timesYourSalary * (1 - 0.18))
-  const taxes = Math.floor( timesYourSalary * numberOfHoursYouWorked * 0.18)
+  const pay = Math.round(numberOfHoursYouWorked * timesYourSalary * (1 -numberOfTax))
+  const taxes =Math.round( timesYourSalary * numberOfHoursYouWorked * numberOfTax)
 
   // output
-  document.getElementById('pay').innerHTML ='Your pay will be:'  + ' $'+pay 
-  document.getElementById('taxes').innerHTML ='the govenment will take:' + ' $'+taxes
+  document.getElementById('pay').innerHTML ='Your pay will be:'  + ' $'+pay.toFixed(2); 
+  document.getElementById('taxes').innerHTML ='the govenment will take:' + ' $'+taxes.toFixed(2);
 }
