efe080f93c1a0574bc2864c546fb594c56b0675f
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-05-12 00:35:28 +0000

    done

--- js/script.js
+++ js/script.js
@@ -2,19 +2,22 @@
 //
 // Created by: Tymofii
 // Created on: Sep 2020
+// This file contains the JS functions for index.html
 
-"use strict"
 
-function calculatePayAndTaxes () {
-  // input
-  const numberOfHoursYouWorked = parseInt(document.getElementById('number-of-hours-you-worked').value)
-  const timesYourSalary= parseInt(document.getElementById('times-your-salary').value)
-  const numberOfTax = 0.18;
-  // process
-  const pay = (numberOfHoursYouWorked * timesYourSalary * (1 -numberOfTax))
-  const taxes =( timesYourSalary * numberOfHoursYouWorked * numberOfTax)
+"use strict";
 
-  // output
-  document.getElementById('pay').innerHTML ='Your pay will be:'  + ' $'+pay.toFixed(2); 
-  document.getElementById('taxes').innerHTML ='the govenment will take:' + ' $'+taxes.toFixed(2);
+function calculatePayAndTaxes() {
+    // input
+    const numberOfHoursYouWorked = parseFloat(document.getElementById('number-of-hours-you-worked').value);
+    const timesYourSalary = parseFloat(document.getElementById('times-your-salary').value);
+    const taxRate = 0.18;
+
+    // process
+    const pay = (numberOfHoursYouWorked * timesYourSalary) * (1 - taxRate);
+    const taxes = (numberOfHoursYouWorked * timesYourSalary) * taxRate;
+
+    // output
+    document.getElementById('pay').innerHTML = 'Your pay will be: $' + pay.toFixed(2);
+    document.getElementById('taxes').innerHTML = 'The government will take: $' + taxes.toFixed(2);
 }
